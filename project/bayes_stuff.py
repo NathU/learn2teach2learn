acdf019a16dec1a7502530802a83eefc73cc38d7
Nathan Ulmer <Nathan Ulmer>
2017-03-16 17:40:46 -0600

    Got some basic naive bayes working

--- project/bayes_stuff.py
+++ project/bayes_stuff.py
@@ -1,7 +1,18 @@
+# ------------------------ NOTES ------------------------ 
+# 
+# We're looking to solve:
+#	1) Prob of Client given month:  
+#		P(Client|Month) = P(Client,Month) / P(Month)
+#
+#
+#
+
+
 
 import numpy
 import sys
 import datetime
+from math import log, exp
 
 ID = 0			# numeric
 F_INICIO = 1	# numeric
@@ -15,47 +26,66 @@ ESTADO = 8
 ID_TECNICO = 9	# numeric
 DURATION = 10	# numeric
 
+
+calc_individual = False
+
 def main(argv):
+	# ------------------------------- Preprocessing --------------------------
 	filename = "bayes_me.csv"
 	file = open("./"+filename, "r")
 	raw_data = (file.read()).split("\n")
 	file.close()
 	data = list(map((lambda row: row.split(",")), raw_data))
 	
-	#for i in range(7):
-		#print(data[i])
-		
-		
-	# Prob of Client given month.
 	#start_months = list(set(list(map(int, list(set(list(numpy.transpose(data))[F_INICIO]))))))
-	#clients = list(set(list(map(int, list(set(list(numpy.transpose(data))[ID_CLIENTE]))))))
+	clients = list(set(list(map(int, list(set(list(numpy.transpose(data))[ID_CLIENTE]))))))
 	
-	month_client_dict = {}
+	month_client_dict = {} #
 	for m in range(1,13):
 		month_client_dict[str(m)] = {}
-	#print(month_client_dict)
-	for row in data:
-		month_client_dict[row[F_INICIO]][row[ID_CLIENTE]] = 0
-	#print(month_client_dict)
+		for c in clients:
+			month_client_dict[str(m)][str(c)] = 0
+
 	for row in data:
 		month_client_dict[row[F_INICIO]][row[ID_CLIENTE]] += 1	
-	#print(month_client_dict)
 	
-	month_totals = {}
-	total_transactions = 0
+	trans_per_client = {} #
+	for c in clients:
+		trans_per_client[str(c)] = 0
+		
+	trans_per_month = {} #
 	for m in range(1,13):
 		sum = 0
-		for t in month_client_dict[str(m)]:
-			sum += month_client_dict[str(m)][t]
-		month_totals[str(m)] = sum
-		total_transactions += sum
-	print(month_totals)
-	print("Rows: "+str(len(data)))
-	print("Transactions Counted: "+str(total_transactions))
-	
-	# Now Implement Naive Bayes...
+		for c in month_client_dict[str(m)]:
+			sum += month_client_dict[str(m)][c]
+			trans_per_client[c] += month_client_dict[str(m)][c]
+		trans_per_month[str(m)] = sum
+	# ---------------------------------------------------------------------------
 	
 	
+	
+	M = input("Month: ")
+	p_M = trans_per_month[M] / len(data)
+	
+	if calc_individual:
+		#print(clients)
+		C = input("Client: ")
+		joint_p_CM = month_client_dict[M][C] / len(data)
+		bayes1 = joint_p_CM / p_M
+		#print(str(joint_p_CM)+" / "+str(p_M)+" = "+str(bayes1)+"  ... right?")
+		print("Probability of transaction with Client #"+C+" during Month #"+M+" = "+str(bayes1))
+	
+	else:
+		probs = list(map(lambda c: (month_client_dict[M][str(c)] / len(data)) / (trans_per_month[M] / len(data)), clients))
+		
+		client_probs = {}
+		for c, p in zip(clients, probs):
+			client_probs[c] = p
+		
+		lim = int(input("View top <how many?> most likely Clients: "))
+		print("-------------------------------")
+		for k, i in zip(sorted(client_probs, key=client_probs.get, reverse=True), range(lim)):
+			print("client "+str(k)+": "+str(client_probs[k]))	
 	
 	
 	
