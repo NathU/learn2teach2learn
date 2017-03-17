3d0fcf7b09aaf70f0285d7bec5a9c6c36109725a
Nathan Ulmer <Nathan Ulmer>
2017-03-17 16:23:52 -0600

    demo on rule mining...

--- project/rule_mining.R
+++ project/rule_mining.R
@@ -1,13 +1,15 @@
+# salesman, client ---> tipo_t
+# salesman, client ---> estado
+
 library(arules)
 mydata = read.transactions("C:/.conda/envs/cs450/course_work/project/rule_mine_me.csv", sep = ",")
-op_rules = apriori(mydata, parameter = list(support = .005, confidence = .005, minlen = 2, maxlen = 4))
-interesting = subset(op_rules,lhs %pin% "start.")
-interesting = subset(interesting,rhs %pin% "client.")
-inspect(sort(interesting, by="support"))
+op_rules = apriori(mydata, parameter = list(support = .001, confidence = .001, minlen = 2, maxlen = 3))
 
+interesting = subset(op_rules,lhs %in% "salesman.15")
+#interesting = subset(op_rules,lhs %pin% "client.")
 
+#tipo_t = c("Retiro", "Prestamo", "Cobranza", "Soporte", "Venta", "Arriendo")
+#estado =c("Terminada", "Prestado", "Anulada", "Facturacion", "Armado", "Contabilidad")
 
-
-more = subset(interesting, rhs %pin% "client.")
-
-inspect(sort(more, by="support"))
+interesting = subset(interesting,rhs %in% "Anulada")
+inspect(sort(interesting, by="lift"))
